2e22d69488302f28f621a379adc86b40bde62c77
stephanieteixeirabcba-lab <stephanieteixeira.bcba@gmail.com>
2026-05-09 16:38:30 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './about.html'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
